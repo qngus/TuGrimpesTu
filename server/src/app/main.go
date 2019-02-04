37be2f8d896418ac5422f6c9bc214cbeb356aa67
Clément Levasseur <clevasseur@ad.gbs>
2019-02-03 19:01:50 -0500

    Initializing project with go and mongoDB

---
+++ server/src/app/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+  "log"
+  "net/http"
+)
+func main() {
+  http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+  log.Println("Hello Mars!")
+  }))
+  log.Println("Now server is running on port 3000")
+  http.ListenAndServe(":3000", nil)
+}
